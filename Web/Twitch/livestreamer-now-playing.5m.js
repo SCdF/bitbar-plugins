1f24828982d61358e7da67a633bb49cde0e64686
Stefan du Fresne <stefan@sdufresne.info>
2016-04-07 23:17:29 +0100

    Auth key now comes from livestreamer
    Instead of hardcoding we just piggyback on livestreamer, either pulling the key
    from their config or using their hook to authenticate with twitch

--- Web/Twitch/livestreamer-now-playing.5m.js
+++ Web/Twitch/livestreamer-now-playing.5m.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env /Users/scdf/.nvm/versions/node/v5.4.0/bin/node
 // TODO: ^^ change this to where node should be, and fix my local install
-// TODO: consider re-writing in something more accessible such as python
 
 // <bitbar.title>Twitch Now Playing</bitbar.title>
 // <bitbar.version>v0.5</bitbar.version>
@@ -10,57 +9,75 @@
 // <bitbar.image>https://i.imgur.com/PznEQCt.png</bitbar.image>
 // <bitbar.dependencies>node, livestreamer</bitbar.dependencies>
 
-// TODO: load this from a generic storage location
-// TODO: detect when we don't have this and provide a link to get it
-var ACCESS_TOKEN = "PUT YOUR TOKEN HERE";
-var urlHost = 'api.twitch.tv'
-var urlPath = '/kraken/streams/followed?stream_type=live'
-var icon = '👾';
-var bleedPurple = '💜';
 var LIVESTREAMER_PATH = '/usr/local/bin/livestreamer';
-var STATUS_LENGTH = 25;
+var LIVESTREAMER_CONFIG_PATH = process.env.HOME + '/.config/livestreamer/config';
+var AUTH_PROP_KEY = 'twitch-oauth-token';
+var ACCESS_TOKEN = readAccessToken();
 
-function handleResponse(body) {
-    var streamByGame = {}
-    for(stream of body.streams) {
-        if (!streamByGame[stream.channel.game]) {
-            streamByGame[stream.channel.game] = [];
+function readAccessToken() {
+    try {
+        var data = require('fs').readFileSync(LIVESTREAMER_CONFIG_PATH, 'utf8');
+        if (data) {
+            var line = data.split('\n').find(function(line) {
+                return line.indexOf(AUTH_PROP_KEY) >= 0;
+            });
+
+            return line.substring(line.indexOf('=') + 1);
         }
-
-        streamByGame[stream.channel.game].push(stream);
-    }
-
-    var outputs = [];
-
-    for (game in streamByGame) {
-        outputs.push(['', game, '| size=10 \n', streamByGame[game].map(function(stream) {
-            var channel = stream.channel;
-            var url = channel.url.replace('http://',    '');
-            return  [channel.display_name, ' | size=12 terminal=false bash=' + LIVESTREAMER_PATH + ' param1=', url, '\n'].join('') +
-                    ['[', stream.viewers, '] ', channel.status, '| size=12 alternate=true length=30 bash=' + LIVESTREAMER_PATH + ' param1=', url, '\n'].join('');
-        }).join('')].join(''));
-    }
-
-    console.log(
-        icon + ' ' +
-        (body.streams.length > 0 ? body.streams.length : '') +
-        '\n---\n' +
-        outputs.join('\n---\n'));
+    } catch (e) {}
 }
 
-var https = require('https');
-https.get({
-    hostname: urlHost,
-    path: urlPath,
-    headers: {
-        'Authorization': 'OAuth ' + ACCESS_TOKEN
+if (ACCESS_TOKEN) {
+    var urlHost = 'api.twitch.tv'
+    var urlPath = '/kraken/streams/followed?stream_type=live'
+    var icon = '👾';
+    var STATUS_LENGTH = 25;
+
+    function handleResponse(body) {
+        var streamByGame = {};
+        for(stream of body.streams) {
+            if (!streamByGame[stream.channel.game]) {
+                streamByGame[stream.channel.game] = [];
+            }
+
+            streamByGame[stream.channel.game].push(stream);
+        }
+
+        var outputs = [];
+
+        for (game in streamByGame) {
+            outputs.push(['', game, '| size=10 \n', streamByGame[game].map(function(stream) {
+                var channel = stream.channel;
+                var url = channel.url.replace('http://',    '');
+                return  [channel.display_name, ' | size=12 terminal=false bash=' + LIVESTREAMER_PATH + ' param1=', url, '\n'].join('') +
+                        ['[', stream.viewers, '] ', channel.status, '| size=12 alternate=true length=30 bash=' + LIVESTREAMER_PATH + ' param1=', url, '\n'].join('');
+            }).join('')].join(''));
+        }
+
+        console.log(
+            icon + ' ' +
+            (body.streams.length > 0 ? body.streams.length : '') +
+            '\n---\n' +
+            outputs.join('\n---\n'));
     }
-}, function(res) {
-    var body = '';
-    res.on('data', function(data) {
-        body += data;
+
+    require('https').get({
+        hostname: urlHost,
+        path: urlPath,
+        headers: {
+            'Authorization': 'OAuth ' + ACCESS_TOKEN
+        }
+    }, function(res) {
+        var body = '';
+        res.on('data', function(data) {
+            body += data;
+        });
+        res.on('end', function() {
+            handleResponse(JSON.parse(body));
+        });
     });
-    res.on('end', function() {
-        handleResponse(JSON.parse(body));
-    });
-});
+} else {
+    console.log('💔');
+    console.log('---');
+    console.log('Click to authenticate livestreamer | terminal=false bash=' + LIVESTREAMER_PATH + ' param1=--twitch-oauth-authenticate');
+}
